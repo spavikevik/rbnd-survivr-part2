f802a83e675666b0a83ce947bdd3bb9655aaf0ea
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2016-04-25 14:20:32 +0200

    Finished Jury class. All jury tests passing.

--- lib/jury.rb
+++ lib/jury.rb
@@ -10,6 +10,22 @@ class Jury
   end
 
   def cast_votes(contestants)
-    {}
+    votes = Hash[contestants.map {|c| [c, 0]}]
+    @members.each do |member|
+      votes[c = votes.keys.sample] += 1
+      puts "#{member} voted for #{c}."
+    end
+    return votes
+  end
+
+  def report_votes(votes)
+    votes.each do |c, v|
+      puts "#{c} has #{v} votes."
+    end
+    return
+  end
+
+  def announce_winner(votes)
+    votes.max_by{|c, v| v}[0]
   end
 end
